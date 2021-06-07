3d2472b5970f8e724bc0b9574b4d361ea9f1611d
RamonLzSouza <ramonsouza0309@gmail.com>
2021-06-07 01:03:48 -0300

    add_drawer

--- Desenvolvimento/moda_us/lib/main.dart
+++ Desenvolvimento/moda_us/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:moda_us/screens/base/base_screen.dart';
 
 void main() {
   runApp(MyApp());
 
-  
 }
 
 class MyApp extends StatelessWidget {
@@ -12,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Moda Us',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      home: BaseScreen(),
     );
   }
 }
---
+++ Desenvolvimento/moda_us/lib/screens/base/base_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:moda_us/common/custom_drawer/custom_drawer.dart';
+
+class BaseScreen extends StatelessWidget {
+
+  final PageController pageController = PageController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: pageController,
+      physics: const NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Scaffold(
+          drawer: CustomDrawer(),
+          appBar: AppBar(
+            title: const Text('Home')
+          ),
+        ),
+        Container(color: Colors.red,),
+        Container(color: Colors.yellow,),
+        Container(color: Colors.green,),
+        
+      ],
+      
+    );
+  }
+}
