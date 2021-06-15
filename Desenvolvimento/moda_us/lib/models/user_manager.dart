eace5c7820faeba408cdcd27b19e4314434570d5
RamonLzSouza <ramonsouza0309@gmail.com>
2021-06-14 23:22:48 -0300

    bloqueando_campos_login

--- Desenvolvimento/moda_us/lib/models/user_manager.dart
+++ Desenvolvimento/moda_us/lib/models/user_manager.dart
@@ -1,21 +1,31 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:moda_us/helpers/firebase_erros.dart';
 import 'package:moda_us/models/user.dart';
 
-class UserManager {
+class UserManager extends ChangeNotifier {
 
   final FirebaseAuth auth = FirebaseAuth.instance;
 
-  Future<void> signIn({User user, Function onFail, Function onSucess}) async {
+  bool loading = false;
+
+  Future<void> signIn({User user, Function onFail, Function onSuccess}) async {
+    setLoading(true);
     try {
       final AuthResult result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
 
-      onSucess();
+      onSuccess();
     } on PlatformException catch (e){
       onFail(getErrorString(e.code));
     }
+    setLoading(false);
+  }
+
+  void setLoading(bool value){
+    loading = value;
+    notifyListeners();
   }
 
 }
