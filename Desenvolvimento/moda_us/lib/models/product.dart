b85625c04ee37cc07f7f1f73308e3964dbfdf6ae
RamonLzSouza <ramonsouza0309@gmail.com>
2021-06-28 23:31:27 -0300

    price_product

--- Desenvolvimento/moda_us/lib/models/product.dart
+++ Desenvolvimento/moda_us/lib/models/product.dart
@@ -1,15 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'item_size.dart';
+
 class Product {
   Product.fromDocument(DocumentSnapshot document) {
     id = document.documentID;
     name = document['name'] as String;
     description = document['description'] as String;
     images = List<String>.from(document.data['images'] as List<dynamic>);
+    sizes = (document.data['sizes'] as List<dynamic> ?? []).map((s) => ItemSize.fromMap(s as Map<String, dynamic>)).toList();
+
+    print(sizes);
   }
 
   String id;
   String name;
   String description;
   List<String> images;
+  List<ItemSize> sizes;
 }
